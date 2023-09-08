e200b7f22ab56af072c609c1391502c8c56ca3b5
David Davis <David28106@outlook.com>
2023-09-08 16:52:34 -0400

    Bootloader segment register preset for compatibility

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -1,8 +1,19 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+jmp 0x7c0:start ; change code segment address to 0x7c00
 
 start:
+    
+    cli ; Clear interrupts flag prevent interrupting of segment register changes
+    mov ax, 0x7c0 ; boot loading to 0x7c00 (0x7c0 * 16 or shift left to 0x7c00 + ORG which is 0)
+    mov ds, ax ; setting data, extra segment to 0x7c00
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; Enable interrupts
+
     mov si, message
     call print
     jmp $
@@ -23,6 +34,6 @@ print_char:
     ret
 
 
-message: db 'Hello World', 0
+message: db 'Hello World', 0 ; data segment(because of db) + ORG(location related to current segment) + offset within
 times 510-($ - $$) db 0
-dw 0xAA55
+dw 0xAA55 ; signature
