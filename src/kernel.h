c441a8db68d23edff6e36188bf0918893a5ddb7e
David Davis <David28106@outlook.com>
2023-09-15 23:38:06 -0400

    refactoring for print

--- src/kernel.h
+++ src/kernel.h
@@ -1,10 +1,9 @@
 #ifndef KERNEL_H
 #define KERNEL_H
-#endif
 
-#define VGA_ADDRESS 0xb8000
-#define VGA_HEIGHT 20
-#define VGA_LENGTH 80
-#define WHITE_ON_BLACK 0x0f
+#include <stddef.h>
+#include <stdint.h>
 
 void kernel_main(void);
+
+#endif
